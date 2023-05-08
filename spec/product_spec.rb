2975d39ce2f0dc214d0133fdb0cdcbe04c33ea22
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-08 13:44:44 -0400

    Test new product with no name is invalid

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -6,7 +6,11 @@ RSpec.describe Product, type: :model do
       @product = Product.new(name: "Prod name", price: 1000, quantity: 5, category: Category.new())
       expect(@product).to be_valid
     end
-    it "is not valid with name missing"
+    
+    it "is not valid with name missing" do
+      @product = Product.new(price: 1000, quantity: 5, category: Category.new())
+      expect(@product).to_not be_valid
+    end
     it "is not valid with price missing"
     it "is not valid with quantity missing"
     it "is not valid with category missing"
