f175114ec34be8379a9e0af792a23c45a708175a
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-08 13:55:16 -0400

    Add assertions that check for appropriate error messages

--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -5,26 +5,31 @@ RSpec.describe Product, type: :model do
     it "is valid with valid attributes" do
       @product = Product.new(name: "Prod name", price: 1000, quantity: 5, category: Category.new())
       expect(@product).to be_valid
+      expect(@product.errors.full_messages).to match_array([])
     end
     
     it "is not valid with name missing" do
       @product = Product.new(name: nil, price: 1000, quantity: 5, category: Category.new())
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
-
+    
     it "is not valid with price missing" do
       @product = Product.new(name: "Prod name", price_cents: nil, quantity: 5, category: Category.new())
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
     end
-
+    
     it "is not valid with quantity missing" do
       @product = Product.new(name: "Prod name", price: 1000, quantity: nil, category: Category.new())
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
-
+    
     it "is not valid with category missing" do
       @product = Product.new(name: "Prod name", price: 1000, quantity: 5, category: nil)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
   end
