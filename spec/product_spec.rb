523557f2f811dd18aba7680a71005386ed547693
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-08 13:43:29 -0400

    Test new product validates with everything present

---
+++ spec/product_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it "is valid with valid attributes" do
+      @product = Product.new(name: "Prod name", price: 1000, quantity: 5, category: Category.new())
+      expect(@product).to be_valid
+    end
+    it "is not valid with name missing"
+    it "is not valid with price missing"
+    it "is not valid with quantity missing"
+    it "is not valid with category missing"
+
+  end
+end
