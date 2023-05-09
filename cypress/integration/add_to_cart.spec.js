0d3d60ee404b5ab49b7ad553f94a51106d9ee18f
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-09 16:30:39 -0400

    Add add to card spec file

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -1,5 +1,5 @@
 
-describe('product details page', () => {
+describe('product add to cart page', () => {
   it('loads the home page and clicks on one of the products', () => {
     cy.visit('/').get(".products article").first().click();
   })
