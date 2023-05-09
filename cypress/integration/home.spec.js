b2a4a76ada53d5a9cc41bbd81d82ead658c2ebd2
KovaKreative <eric.kovalevskyy@gmail.com>
2023-05-09 15:54:34 -0400

    Test home page in cypress

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -15,4 +15,13 @@ describe('jungle app', () => {
   it('displays the home page', () => {
     cy.visit('/').contains('Looking for a way to add some life to your home?');
   })
+
+  it("displays products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  })
+
+  it("loads 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+  
 })
